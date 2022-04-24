af56230ad990545dfb577f46916c1a3fca052601
Baisakhi Das <60517623+baisakhidas11@users.noreply.github.com>
2022-04-24 20:14:40 +0530

    Create prog.js

---
+++ prog.js
@@ -0,0 +1,17 @@
+
+var d = new Date();
+d.setDate(d.getDate() + 5);
+
+var m = new Date();
+m.setMonth(m.getMonth() + 5);
+
+var y = new Date();
+y.setFullYear(y.getFullYear() + 5);
+
+if(const days=15){
+const nextDate = new Date( Date.now() + days * 24 * 60 * 60 * 1000)
+};
+else
+{
+return 0;
+}
